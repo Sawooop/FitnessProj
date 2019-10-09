7dba30105ee36c4366dea03dfa04459674930d5f
wynne.patrickt@gmail.com <wynne.patrickt@gmail.com>
2019-10-09 15:57:30 -0400

    test?

---
+++ FitnessProj/FitnessProj/FitnessType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProj
+{
+    class FitnessType
+    {
+        public string name;
+        public string unitName;
+        public int unitWorth;
+
+        public FitnessType(string Name, string UnitName, int UnitWorth)
+        {
+            name = Name;
+            unitName = UnitName;
+            unitWorth = UnitWorth;
+        }
+    }
+}
