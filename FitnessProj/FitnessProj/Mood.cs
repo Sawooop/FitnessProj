e0e1a9437405962457efbd3013fdbe612f125e5a
jackson <langjackson.jl@gmail.com>
2019-10-15 19:13:55 -0400

    gg

---
+++ FitnessProj/FitnessProj/Mood.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProj
+{
+    class Mood
+    {
+        int moodLvl;
+        int totalPoints;
+        PointsNeeded need = new PointsNeeded();
+        FitnessType totalPointsInTotal = new FitnessType();
+        public int TotalPoints()
+        {
+            totalPoints = totalPointsInTotal.TotalWorth();
+            return totalPoints;
+        }
+
+        public int MoodLevel()
+        {
+            moodLvl = totalPoints / need.PointsToBeHappy();
+            return moodLvl;
+        }
+        public double ProgressToNextLvl()
+        {
+            return (Convert.ToDouble(totalPoints / need.PointsToBeHappy()) - moodLvl) * 100;
+        }
+    }
+}
---
+++ FitnessProj/FitnessProj/PointsNeeded.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProj
+{
+    class PointsNeeded
+    {
+        int pointsToBeNeeded;
+
+        public PointsNeeded()
+        {
+
+        }
+
+        public int PointsToBeHappy()
+        {
+            return 5000;
+        }
+    }
+}
