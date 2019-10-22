aec84c482a5b63f7ec5f56fb72e562d37175f8e2
jackson <langjackson.jl@gmail.com>
2019-10-22 18:24:35 -0400

    Ready for test

--- FitnessProj/FitnessProj/FitnessType.cs
+++ FitnessProj/FitnessProj/FitnessType.cs
@@ -13,11 +13,12 @@ namespace FitnessProj
         public int unitWorth;
         public int numberOfUnits;
 
-        public FitnessType(string Name, string UnitName, int UnitWorth)
+        public FitnessType(string Name, string UnitName, int UnitWorth, int NumberOfUnits)
         {
             name = Name;
             unitName = UnitName;
             unitWorth = UnitWorth;
+            numberOfUnits = NumberOfUnits;
         }
 
         public FitnessType()
